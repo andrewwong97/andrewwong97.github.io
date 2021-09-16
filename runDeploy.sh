c83771b4ee704de143fa3b3d210e77cc7d43fc73
Andrew Wong <awong3@atlassian.com>
2021-09-16 15:22:45 -0700

    Update deploy script

--- runDeploy.sh
+++ runDeploy.sh
@@ -7,4 +7,5 @@ rm -rf build;
 python addSPA.py;
 git add static
 git commit -am "Update deploy";
-git push;
+git push https://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@github.com/andrewwong97/andrewwong97.github.io.git
+
